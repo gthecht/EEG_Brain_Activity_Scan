98d1261488c93cfb11e00561e58745794d3f8aca
RahamimRonen <rahamim.ronen@gmail.com>
2017-06-05 18:56:36 +0300

    changes in the way of finding the relevant electrodes.

--- EEG_code/Finding_good_electrodes/find_relevant_electrodes.m
+++ EEG_code/Finding_good_electrodes/find_relevant_electrodes.m
@@ -2,9 +2,10 @@ function [ good_elec ] = find_relevant_electrodes( elec_dat, threshold )
 % This function find the good electrodes which do not runs out of the
 % median value by "threshold" times of it
 
-med_by_row    = median(elec_dat);
-median_of_dat = median(med_by_row);
-out_of_bound  = (abs(elec_dat) > threshold * abs(median_of_dat));
+med_by_time   = median(abs(elec_dat));
+[row, ~]      = size(elec_dat); 
+% median_of_dat = median(med_by_time);
+out_of_bound  = (abs(elec_dat) > threshold * abs(repmat(med_by_time,row,1)));
 bad_elec      = sum(out_of_bound, 2);
 good_elec     = find(bad_elec == 0);
 
