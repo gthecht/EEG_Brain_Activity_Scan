662ae4f91b30d79ef9d9ef379761e148353a7a51
gthecht <gdpstd@gmail.com>
2017-11-20 18:46:10 +0200

    fixing SVM

--- EEG_code/SVM/get_training_set_for_all_types.m
+++ EEG_code/SVM/get_training_set_for_all_types.m
@@ -11,5 +11,21 @@ for jj = 1:types_num
     test_mat((jj-1)*n+1 : jj*n,:) = test_train(n+1:2*n,:);
 end
 [all_classifier, validationAccuracy] = train_all_classifier(train_mat);
+%%
 test_labels  = all_classifier.predictFcn(test_mat(:,1:3));
-confmat_all  = confusionmat(test_labels, test_mat(:, end));
+confmat_all  = confusionmat(test_labels, test_mat(:, end-1));
+%%
+figure();
+imshow(confmat_all, 'InitialMagnification',100);
+colormap(summer);
+caxis([0,max(confmat_all(:))]);
+%%
+stim_confusion = zeros(9);
+for jj = 1:16
+    stim_confusion = stim_confusion + confmat_all(9*(jj-1) + 1:9*jj, 9*(jj-1) + 1:9*jj);
+end
+figure();
+imshow(stim_confusion, 'InitialMagnification',10000);
+colormap(summer);
+caxis([0,max(confmat_all(:))]);
+
